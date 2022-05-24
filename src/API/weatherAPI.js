207e313d0167d9811c7a281a2343e3d3a3a207aa
Maxim Kudryashov <maximkudryashov969@gmail.com>
2022-05-24 21:56:38 +0300

    added weather api

--- src/API/weatherAPI.js
+++ src/API/weatherAPI.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://api.openweathermap.org/data/2.5/weather?'
+    baseURL: 'https://api.openweathermap.org/data/2.5'
 })
 
 const APIkey = '1b67b174bff4b9ae4f3d5df0c09b693d'
 
 const weatherAPI = {
     getWeatherByCity: (city) => {
-        return instance.get(`q=${city}&appid=${APIkey}&lang={ru}`)
+        return instance.get(`weather?q=${city}&lang=ru&appid=${APIkey}`)
             .catch(err => console.log(err))
     },
 };
--- src/Components/pages/WeatherPage/WeatherPage.jsx
+++ src/Components/pages/WeatherPage/WeatherPage.jsx
@@ -1,35 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import style from "./WeatherPage.module.css";
 import weatherAPI from "../../../API/weatherAPI";
 
 const WeatherPage = () => {
-    const [queryText, setQueryText] = useState('');
-    const [data, setData] = useState({});
+  const [queryText, setQueryText] = useState("");
+  const [data, setData] = useState({});
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const response = await weatherAPI.getWeatherByCity(queryText)
-        console.log(response)
-        setQueryText('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await weatherAPI.getWeatherByCity(queryText);
+
+    if (response.status === 200) {
+      setData(response.data);
+      setQueryText("");
     }
+  };
 
-    return (
-        <div>
-            <h1>Прогноз погоды</h1>
-            <form
-                className={style.form}
-                onSubmit={handleSubmit}
-            >
-                <input
-                    placeholder="Введите название города"
-                    value={queryText}
-                    onChange={(e) => setQueryText(e.target.value)}
-                />
-                <button>Найти</button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Прогноз погоды</h1>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <input
+          placeholder="Введите название города"
+          value={queryText}
+          onChange={(e) => setQueryText(e.target.value)}
+        />
+        <button>Найти</button>
+      </form>
+      <h2>{data.name}</h2>
+    </div>
+  );
 };
 
 export default WeatherPage;
