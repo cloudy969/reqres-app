c66b887255f51573fba692d6f0e5d1211aa80572
Maxim Kudryashov <maximkudryashov969@gmail.com>
2022-05-24 13:52:40 +0300

    added custom pagination and valid page numbers in url

--- src/Components/UsersPage/UsersList/UsersList.jsx
+++ src/Components/UsersPage/UsersList/UsersList.jsx
@@ -1,61 +1,74 @@
-import React from 'react';
-import UserItem from "../UserItem/UserItem";
-import {Pagination, Table} from 'antd';
-import style from './UserList.module.css'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Table } from "antd";
 
-const UsersList = ({users, pageSize, totalUsers, currentPage, setCurrentPage}) => {
+import { API } from "../../../API/API";
+import style from "./UserList.module.css";
+import Pagination from "../../UI/Pagination/Pagination";
 
-    const columns = [
-        {
-            title: 'Аватар',
-            dataIndex: 'avatar',
-            key: 'avatar',
-            render: (url) => <img className={style.userAvatar} src={url}/>,
-        },
-        {
-            title: 'Имя',
-            dataIndex: 'first_name',
-            key: 'first_name',
-            render: name => <a href='#'>{name}</a>
-        },
-        {
-            title: 'Фамилия',
-            dataIndex: 'last_name',
-            key: 'last_name',
-        },
-        {
-            title: 'Email',
-            dataIndex: 'email',
-            key: 'email',
-        }
-    ]
-    const dataSource = users;
+const UsersList = ({
+  users,
+  pageSize,
+  totalUsers,
+  setUsers,
+  pageNumber,
+  page,
+  setPage,
+}) => {
+  const navigate = useNavigate();
 
-    const changePage = (pageNumber) => {
-        setCurrentPage(pageNumber)
-        console.log(currentPage)
-    }
+  const columns = [
+    {
+      title: "Аватар",
+      dataIndex: "avatar",
+      key: "avatar",
+      render: (url) => <img className={style.userAvatar} src={url} />,
+    },
+    {
+      title: "Имя",
+      dataIndex: "first_name",
+      key: "first_name",
+      render: (name) => <a href="#">{name}</a>,
+    },
+    {
+      title: "Фамилия",
+      dataIndex: "last_name",
+      key: "last_name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+  ];
+  const dataSource = users;
 
-    return (
-        <div>
-            <Table
-                className={style.wrapper}
-                columns={columns}
-                dataSource={dataSource}
-                showHeader={false}
-                pagination={false}
-            />
-            {/*<ul className='users-list'>*/}
-            {/*    {users.map(user => <li key={user.id}> <UserItem {...user} /> </li>)}*/}
-            {/*</ul>*/}
-            <Pagination current={currentPage}
-                        pageSize={pageSize}
-                        total={totalUsers}
-                        onChange={changePage}
-                        className={style.pagination}
-            />
-        </div>
-    );
+  useEffect(async () => {
+    const response = await API.getUsers(page);
+    setUsers(response.data.data);
+
+    page === 1
+      ? navigate("/users", { replace: true })
+      : navigate(`/users?page=${page}`, { replace: true });
+  }, [page]);
+
+  return (
+    <div>
+      <Table
+        className={style.wrapper}
+        columns={columns}
+        dataSource={dataSource}
+        showHeader={false}
+        pagination={false}
+      />
+      <Pagination
+        pageNumber={pageNumber}
+        totalPages={totalUsers / pageSize}
+        page={page}
+        setPage={setPage}
+      />
+    </div>
+  );
 };
 
 export default UsersList;
--- src/Components/UsersPage/UsersPage.jsx
+++ src/Components/UsersPage/UsersPage.jsx
@@ -1,29 +1,46 @@
-import React, {useEffect, useState} from 'react';
-import {API} from "../../API/API";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import { API } from "../../API/API";
 import UsersList from "./UsersList/UsersList";
+import {set} from "react-hook-form";
 
 const UsersPage = () => {
+  const location = useLocation();
+  const pageNumber = location.search?.substring(6) || 1;
 
-    const [users, setUsers] = useState([]);
-    const [paginationProps, setPaginationProps] = useState({totalUsers: 0, pageSize: 10 })
-    const [currentPage, setCurrentPage] = useState(1)
+  const [users, setUsers] = useState([]);
+  const [paginationProps, setPaginationProps] = useState({
+    totalUsers: 0,
+    pageSize: 10,
+  });
+  const [page, setPage] = useState(parseInt(pageNumber));
 
-    useEffect( async () => {
-        const response = await API.getUsers(currentPage);
-        setUsers(response.data.data)
 
-        setPaginationProps({
-            ...paginationProps,
-            totalUsers: response.data.total,
-            pageSize: response.data.per_page})
-    }, [currentPage])
+  useEffect(async () => {
+    setPage(parseInt(pageNumber));
 
-    return <UsersList users={users}
-                      totalUsers={paginationProps.totalUsers}
-                      pageSize={paginationProps.pageSize}
-                      currentPage={currentPage}
-                      setCurrentPage={setCurrentPage}
+    const response = await API.getUsers(pageNumber);
+    setUsers(response.data.data);
+
+    setPaginationProps({
+      ...paginationProps,
+      totalUsers: response.data.total,
+      pageSize: response.data.per_page,
+    });
+  }, [pageNumber]);
+
+  return (
+    <UsersList
+      users={users}
+      totalUsers={paginationProps.totalUsers}
+      pageSize={paginationProps.pageSize}
+      setUsers={setUsers}
+      pageNumber={pageNumber}
+      page={page}
+      setPage={setPage}
     />
+  );
 };
 
 export default UsersPage;
